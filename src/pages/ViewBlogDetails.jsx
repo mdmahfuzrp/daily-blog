5151e48e6b8fcbd580c526f445e3ac39eb821aca
Mahfuz RP <mahfuzrpsmorg@gmail.com>
2023-11-08 22:17:49 +0600

    modify

--- src/pages/ViewBlogDetails.jsx
+++ src/pages/ViewBlogDetails.jsx
@@ -6,6 +6,7 @@ import EditBlog from "./EditBlog";
 import { useQuery } from "react-query";
 import { useParams } from "react-router-dom";
 import { AuthContext } from "../Provider/AuthProvider";
+import { MagnifyingGlass } from "react-loader-spinner";
 
 const ViewBlogDetails = () => {
   const { id } = useParams();
@@ -13,10 +14,12 @@ const ViewBlogDetails = () => {
   console.log(user);
 
   // Get WishlistCount
-  const { data: blogDetails = [] } = useQuery(
+  const { data: blogDetails = [], isLoading } = useQuery(
     ["my-wishlist"],
     async () => {
-      const res = await fetch(`https://daily-blog-server.vercel.app/blog-details/${id}`);
+      const res = await fetch(
+        `https://daily-blog-server.vercel.app/blog-details/${id}`
+      );
       return res.json();
     },
     {
@@ -43,76 +46,94 @@ const ViewBlogDetails = () => {
       <h1 className="text-primary py-2 px-4 font-semibold text-[20px]  border mb-[20px]">
         View Blog Details
       </h1>
-      <div>
-        <img
-          src={blogDetails?.blogPhoto}
-          className="w-full max-w-[550px] rounded-lg mx-auto max-h-[300px] object-cover"
-          alt=""
-        />
-        <h1 className="text-[24px] capitalize lg:text-[30px] font-bold text-center mt-2 mb-4 text-secondBlack">
-          {blogDetails?.blogTitle}
-        </h1>
-        <div className="flex items-center justify-between w-full">
+
+      {isLoading ? (
+        <>
           <div>
-            <p className="text-secondBlack text-opacity-70 text-[15px] font-semibold">
-              Written by -
-            </p>
-            <div className="flex flex-wrap items-center mb-4 lg:mb-0 mt-1">
-              <img
-                className="object-cover border-2 border-primary mb-1 mr-2 rounded-full shadow w-9 h-9 lg:mb-0"
-                src={
-                  blogDetails?.authorPhoto ||
-                  "https://i.ibb.co/zXw7H0W/360-F-107434511-iar-F2z88c6-Ds6-Algtwot-HSAkt-WCd-YOn7.jpg"
-                }
-              />
+            <img
+              src={blogDetails?.blogPhoto}
+              className="w-full max-w-[550px] rounded-lg mx-auto max-h-[300px] object-cover"
+              alt=""
+            />
+            <h1 className="text-[24px] capitalize lg:text-[30px] font-bold text-center mt-2 mb-4 text-secondBlack">
+              {blogDetails?.blogTitle}
+            </h1>
+            <div className="flex items-center justify-between w-full">
               <div>
-                <h2 className="mr-2 text-[13px] font-medium text-gray-700 dark:text-gray-400">
-                  {blogDetails?.authorName}
-                </h2>
-                <p className="text-[11px] font-medium text-gray-400 dark:text-gray-400">
-                  {" "}
-                  {blogDetails?.currentDate}
+                <p className="text-secondBlack text-opacity-70 text-[15px] font-semibold">
+                  Written by -
                 </p>
+                <div className="flex flex-wrap items-center mb-4 lg:mb-0 mt-1">
+                  <img
+                    className="object-cover border-2 border-primary mb-1 mr-2 rounded-full shadow w-9 h-9 lg:mb-0"
+                    src={
+                      blogDetails?.authorPhoto ||
+                      "https://i.ibb.co/zXw7H0W/360-F-107434511-iar-F2z88c6-Ds6-Algtwot-HSAkt-WCd-YOn7.jpg"
+                    }
+                  />
+                  <div>
+                    <h2 className="mr-2 text-[13px] font-medium text-gray-700 dark:text-gray-400">
+                      {blogDetails?.authorName}
+                    </h2>
+                    <p className="text-[11px] font-medium text-gray-400 dark:text-gray-400">
+                      {" "}
+                      {blogDetails?.currentDate}
+                    </p>
+                  </div>
+                </div>
               </div>
+              {blogDetails?.category && (
+                <div className="border py-[1px] px-3 shadow-sm rounded-full">
+                  {blogDetails?.category}
+                </div>
+              )}
+              {blogDetails?.authorEmail === user?.email && (
+                <div>
+                  <button
+                    type="button"
+                    onClick={() => handleAction(blogDetails)}
+                    className="text-base text-secondBlack duration-150 flex items-center justify-end gap-1 hover:text-primary font-medium"
+                  >
+                    <BiSolidEdit size={20} className="mt-[-2px]" />
+                    Edit Blog
+                  </button>
+                </div>
+              )}
+            </div>
+
+            <div className="mt-[30px] ">
+              <p className="text-secondBlack text-base font-medium flex items-center gap-2">
+                <span className="bg-primary text-white px-[6px] text-[15px] font-medium tracking-wide pb-[2px] rounded-[4px]">
+                  Summery:
+                </span>
+                {blogDetails?.shortDescription}
+              </p>
+            </div>
+
+            <div className="mt-[15px] border  rounded-lg py-2 px-3">
+              <p className="text-secondBlack text-opacity-75 text-base font-medium flex items-center gap-2">
+                {blogDetails?.longDescription}
+              </p>
             </div>
           </div>
-          {blogDetails?.category && (
-            <div className="border py-[1px] px-3 shadow-sm rounded-full">
-              {blogDetails?.category}
-            </div>
-          )}
-          {blogDetails?.authorEmail === user?.email && (
-            <div>
-              <button
-                type="button"
-                onClick={() => handleAction(blogDetails)}
-                className="text-base text-secondBlack duration-150 flex items-center justify-end gap-1 hover:text-primary font-medium"
-              >
-                <BiSolidEdit size={20} className="mt-[-2px]" />
-                Edit Blog
-              </button>
-            </div>
-          )}
+          <div className="mt-[30px]">
+            <Review data={blogDetails} />
+          </div>
+        </>
+      ) : (
+        <div className="w-full col-span-full h-[100px] flex items-center justify-center">
+          <MagnifyingGlass
+            visible={true}
+            height="55"
+            width="55"
+            ariaLabel="MagnifyingGlass-loading"
+            wrapperStyle={{}}
+            wrapperClass="MagnifyingGlass-wrapper"
+            glassColor="white"
+            color="#A27B5C"
+          />
         </div>
-
-        <div className="mt-[30px] ">
-          <p className="text-secondBlack text-base font-medium flex items-center gap-2">
-            <span className="bg-primary text-white px-[6px] text-[15px] font-medium tracking-wide pb-[2px] rounded-[4px]">
-              Summery:
-            </span>
-            {blogDetails?.shortDescription}
-          </p>
-        </div>
-
-        <div className="mt-[15px] border  rounded-lg py-2 px-3">
-          <p className="text-secondBlack text-opacity-75 text-base font-medium flex items-center gap-2">
-            {blogDetails?.longDescription}
-          </p>
-        </div>
-      </div>
-      <div className="mt-[30px]">
-        <Review data={blogDetails} />
-      </div>
+      )}
 
       <SharedModal
         title={modalTitle}
